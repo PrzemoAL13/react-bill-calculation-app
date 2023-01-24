e8720bf0fe9658a693bbce4cbaa1e4e08b959405
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2023-01-24 15:46:11 +0100

    clear project bill calculator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+				<h1>Bill calculator!</h1>
+				<p>Calculate your bill!</p>
+				<form>
+					<div>
+						<p>Price</p>
+						<input type='number' placeholder='Set price...'></input>
+					</div>
+					<div>
+						<p>Tip</p>
+						<input type='number' placeholder='Set tip...'></input>
+					</div>
+					<div>
+						<p>Tax</p>
+						<select>
+							<option value='5'>5%</option>
+							<option value='8'>8%</option>
+							<option selected value='23%'>
+								23%
+							</option>
+						</select>
+					</div>
+					<div>
+						<button>Calculate</button>
+					</div>
+				</form>
+		</div>
+	);
 }
 
 export default App;
