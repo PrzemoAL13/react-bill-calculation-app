5d5d7d54fe5f1b7e34963fff4ea696502dc3d9e6
Przemek Tomaszewski <tomaszewski.przemyslaw1993@gmail.com>
2023-01-31 19:20:20 +0100

    Field select improved , state and default value added

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import FieldSelect from "./components/FieldSelect";
 function App() {
 	const arrTip = [5, 10, 15, 20, 25];
 	const arrTax = [5, 8, 23];
+	const arrTipDefaultElem = -2;
+	
 	return (
 		<div className='App'>
 			<h1>Bill calculator!</h1>
-			<FieldInput
+			{/* <FieldInput
 				inputType='number'
 				placeholderInfo='Set price...'
 				defaultValue='666'
-			/>
+			/> */}
+			<FieldSelect  valueArr={arrTip} defaultValue={arrTipDefaultElem} />
 			{/* <FieldParagraph textParagraph='Calculate your bill!' />
 			<form>
 				<div>
--- src/components/FieldSelect.js
+++ src/components/FieldSelect.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 const FieldSelect = props => {
-	const { valueArr} = props;
-	
+	const { valueArr, defaultValue } = props;
+	const [selectValue, setSelectValue] = useState("");
+	let localDefaultValue =
+		defaultValue < 0 || defaultValue > valueArr.length ? 0 : defaultValue;
+	const handleSelectChange = event => {
+		setSelectValue(event.target.value);
+	};
 	return (
-		<select>
+		<select
+			defaultValue={valueArr[localDefaultValue]}
+			onChange={handleSelectChange}>
 			{valueArr.map(elem => {
 				return (
-					<option key={elem} value={elem}>
+					<option key={elem.toString()} value={elem}>
 						{elem}%
 					</option>
 				);
